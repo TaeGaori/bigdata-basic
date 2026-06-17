127b20d5712f39aaa2727e59ce130c3eb977b628
TaeGaori <xoengur@gmail.com>
2026-06-17 14:05:27 +0900

    6월 17일

--- Practice/Toy_Project.py
+++ Practice/Toy_Project.py
@@ -1,4 +1,4 @@
-class Characher:
+class Character:
 
     def __init__(self, name, hp, attack):
         self.name = name
@@ -10,16 +10,24 @@ class Characher:
     
     def attack_enemy(self, target):
         damage = self.attack
-        if target.is_blocking:
-            damage = damage // 2
+        # if target.is_blocking:
+        #     damage = damage // 2
         target.hp -= damage
         print(f'{self.name}이(가) {target.name}을 공격! {damage}피해!')
-        if target.is_alive() < 0:
-            print("{target.name}이(가) 쓰러졌다!")
+        if not target.is_alive():
+            print(f"{target.name}이(가) 쓰러졌다!")
 
-    def is_alive():
+    def is_alive(self):
         if self.hp > 0:
             return True
         else:
             print(f'{self.name}이(가)  쓰러졌다!')
             return False
+        
+if __name__ == "__main__":
+    hero = Character('아서', hp=150,attack=30)
+    enemy = Character('드레곤', hp=200, attack=40)
+
+    hero.show_info()
+    enemy.show_info()
+    print("-" *40)
