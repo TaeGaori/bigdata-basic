6dfaa18bb5768f45241dfe8483224503dc1e8b89
TaeGaori <xoengur@gmail.com>
2026-06-17 17:44:21 +0900

    6월 17일

--- Practice/Toy_Project.py
+++ Practice/Toy_Project.py
@@ -1,33 +1,49 @@
 class Character:
 
-    def __init__(self, name, hp, attack):
+    def __init__(self, name, hp, attack, job='모험가', shield=0):
         self.name = name
         self.hp = hp
         self.attack = attack
+        self.job = job
+        self.shield = shield
+        self.is_blocking = False
 
     def show_info(self):
-        print(f'이름 : {self.name} | HP : {self.hp} | 공격력 : {self.attack}')
+        print(f'이름 : [self.job] {self.name} | HP : {self.hp} | 공격력 : {self.attack} | 방패 : {self.shield}')
     
     def attack_enemy(self, target):
         damage = self.attack
-        # if target.is_blocking:
-        #     damage = damage // 2
+        if target.is_blocking:
+            damage = damage // 2
         target.hp -= damage
         print(f'{self.name}이(가) {target.name}을 공격! {damage}피해!')
         if not target.is_alive():
             print(f"{target.name}이(가) 쓰러졌다!")
 
     def is_alive(self):
-        if self.hp > 0:
-            return True
+        return self.hp > 0
+
+    def shield_block(self):
+        if self.shield > 0:
+            self.is_blocking = True
+            self.shield -= 1
+            print(f"{self.name}이(가) 방패를 들었다! (남은 횟수 : {self.shield})")
         else:
-            print(f'{self.name}이(가)  쓰러졌다!')
-            return False
-        
+            print(f"{self.name}: 방패가 없어요!")
+
 if __name__ == "__main__":
-    hero = Character('아서', hp=150,attack=30)
-    enemy = Character('드레곤', hp=200, attack=40)
+    hero = Character('아서', hp=150, attack=100, job='전사', shield=3)
+    enemy = Character('드레곤', hp=200, attack=40, job='몬스터')
 
     hero.show_info()
     enemy.show_info()
     print("-" *40)
+
+    hero.shield_block()
+    enemy.attack_enemy(hero)
+    hero.attack_enemy(enemy)
+    hero.attack_enemy(enemy)
+
+    print("-" *40)
+    hero.show_info()
+    enemy.show_info()
